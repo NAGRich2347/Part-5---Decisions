19f73323cb665e26699ec110da3e199f956a8f0e
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-10-16 13:42:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_5___Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userAge;
+
+            Console.WriteLine("Please enter your age");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            if (userAge >= 18)
+            {
+                Console.WriteLine("You are an adult.");
+                Console.ReadLine();
+            }
+                
+
+        }
+    }
+}

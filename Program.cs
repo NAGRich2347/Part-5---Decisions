d9a10c36291876c02d35018a8617e9c2cfacfa48
Noah Richardson <RichNoah983@GoTVDSB.ca>
2020-10-26 13:18:03 -0400

    Having issues with errors while using if statements.

--- Program.cs
+++ Program.cs
@@ -10,17 +10,63 @@ namespace Part_5___Decisions
     {
         static void Main(string[] args)
         {
-            int userAge;
+                int userAge;
 
-            Console.WriteLine("Please enter your age");
-            userAge = Convert.ToInt32(Console.ReadLine());
-            if (userAge >= 18)
-            {
-                Console.WriteLine("You are an adult.");
-                Console.ReadLine();
-            }
-                
+                Console.WriteLine("Please enter your age.");
+                userAge = Convert.ToInt32(Console.ReadLine());
 
+                if (userAge >= 2){
+            
+                    Console.WriteLine("You are an infant.");
+                    Console.ReadLine();
+                }
+
+
+                else (userAge >= 5)
+                {
+                    Console.WriteLine("You are a toddler.");
+                    Console.ReadLine();
+                }
+
+
+                if(userAge >= 12){
+
+                    Console.WriteLine("You are a kid.");
+                    Console.ReadLine();
+                }
+
+
+                else (userAge >= 17){
+            
+                    Console.WriteLine("You are a teen.");
+                    Console.ReadLine();
+                };
+
+                if (userAge >= 18){ 
+            
+                    Console.WriteLine("You are an adult.");
+                    Console.ReadLine();
+                }
+
+                else (userAge >= 40){ 
+            
+                    Console.WriteLine("You are middle-aged.");
+                    Console.ReadLine();
+                };
+
+                if (userAge >= 60){ 
+
+                    Console.WriteLine("You are a senior.");
+                    Console.ReadLine();
+                }
+
+                else (userAge <= 99) {
+
+                    Console.WriteLine("You are ancient!");
+                    Console.ReadLine();
+                };
+
+            
         }
     }
 }
